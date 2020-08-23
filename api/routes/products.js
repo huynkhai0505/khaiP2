04bbef5a70828ee74ab332a4706566f3e66b6bde
huynkhai0505 <huynhkhai0505@gmail.com>
2020-08-23 16:28:04 +0700

    DOC

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const mongoose = require('mongoose');
+
+const Product = require('../models/products');
+
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: "Handling GET request to /products"
@@ -9,8 +13,22 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        namee: req.body.name,
+        price: req.body.price
+    });
+    product.save().then(result => {
+        console.log(result);
+    })
+    .catch (err => console.log(err));
     res.status(201).json({
-        message: "Handling POST request to /products"
+        message: "Handling POST request to /products",
+        createProduct: product
     });
 });
 
--- app.js
+++ app.js
@@ -3,16 +3,38 @@ const express = require('express');
 const app = express();
 
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect('mongodb+srv://huynhkhai:' + process.env.MONGO_ATLAS_PW + '@cluster0.umytc.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+{
+    useMongoClient: true
+});
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*') 
+    res.header('Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    ); 
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //Routes handling requests
 app.use('/products', productRoutes); 
 app.use('/orders', orderRoutes);
 
+//Check for Error
 app.use((req, res, next) => {
     const error = new Error('Not Found');
     error.status = 404 ;
@@ -27,5 +49,7 @@ app.use((error, req , res, next) => {
         }
     });
 });
+
+
 
 module.exports = app; 
